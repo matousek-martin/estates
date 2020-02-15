7b7ce10638619806d6a78d4fc67d784627e4a282
Elisabeth Gabalová <elisabeth@Elisabeths-MacBook-Pro.local>
2020-02-15 23:27:27 +0100

    update to sreality spider

--- sreality_scraping/sreality_scraping/spiders/sreality_spider.py
+++ sreality_scraping/sreality_scraping/spiders/sreality_spider.py
@@ -1,20 +1,19 @@
 import scrapy
 import json
-
+from datetime import datetime
 
 class SrealitySpider(scrapy.Spider):
     name = "sreality"
+    per_page = 500
 
     def start_requests(self):
-        url = "https://www.sreality.cz/api/cs/v2/estates?per_page=500&page=1"
+        url = f"https://www.sreality.cz/api/cs/v2/estates?per_page={self.per_page}&page=1"
         yield scrapy.Request(url=url, callback=self.parse)
 
     def parse(self, response):
         r = json.loads(response.body)
         # parsing links to estates in initial api response
         for estate in r["_embedded"]["estates"]:
-            # hash id as a unique identifier > we do not want to scrape the same estate N times
-            hash_id = estate["hash_id"]
             # url is provided as '/cs/v2/estates/1077595740'
             url_suffix = estate["_links"]["self"]["href"]
             # so we combine it with the prefix from supplied url in self.start_requests
@@ -24,10 +23,10 @@ class SrealitySpider(scrapy.Spider):
                 yield response.follow(url_estate, callback=self.save_estate)
 
         # get number of pages as ... 'Number of total estates/number of estates per page'
-        page_count = r["result_size"] // 500 + 1
+        page_count = r["result_size"] // self.per_page + 1
         # we need to generate new page after we scrape estates on the current one
         # (2, page_count + 1) due to how range works and we already have the first page
-        for page_num in range(2, 5):
+        for page_num in range(2, page_count):
             # split at "&page=\d" with the next page's number and callback to parse
             first_page = response.url.split("&")[0]
             next_page = first_page + f"&page={page_num}"
@@ -35,8 +34,11 @@ class SrealitySpider(scrapy.Spider):
                 yield response.follow(next_page, callback=self.parse)
 
     def save_estate(self, response):
-        data = json.loads(response.body)
         # get estate id from url and save to corresponding json
         estate_id = response.url.split("/")[-1]
+        # construct json to save
+        data = json.loads(response.body)
+        data['estate_id'] = estate_id
+        data['date_scraped'] = datetime.utcnow().strftime("%Y/%m/%d %H:%M:%S")
         with open(f"estates/{estate_id}.json", "w") as js:
             json.dump(data, js)
