1235a4b4b758a21f066ebc016a13221a8f8d8ba6
Elisabeth Gabalová <elisabeth@Elisabeths-MacBook-Pro.local>
2020-02-21 14:46:47 +0100

    spider checks if estate already exists in firestore instead of caching results of previous scrapes

--- sreality_scraping/sreality_scraping/spiders/sreality_spider.py
+++ sreality_scraping/sreality_scraping/spiders/sreality_spider.py
@@ -11,6 +11,7 @@ class SrealitySpider(scrapy.Spider):
     cache = load_cache()
     db = connect_firestore("../notebooks/estates-b0e1b-f074b83c9111.json")
     stopping_count = 0
+
     def start_requests(self):
         url = (
             f"https://www.sreality.cz/api/cs/v2/estates?per_page={self.per_page}&page=1"
@@ -26,7 +27,8 @@ class SrealitySpider(scrapy.Spider):
             # url is provided as '/cs/v2/estates/1077595740'
             url_suffix = estate["_links"]["self"]["href"]
             estate_id = url_suffix.split("/")[-1]
-            if estate_id not in self.cache:
+            estate_exists = self.db.collection(u'sreality').document(estate_id).exists()
+            if not estate_exists:
                 # we combine it with the prefix from supplied url in self.start_requests
                 url_prefix = response.url.split("/cs")[0]
                 url_estate = url_prefix + url_suffix
@@ -37,12 +39,14 @@ class SrealitySpider(scrapy.Spider):
         # (2, page_count + 1) due to how range works and we already have the first page
         for page_num in range(2, page_count):
             # scrape only non-scraped estates
-            if self.stopping_count <= 2000:
+            if self.stopping_count < 20000:
                 # split at "&page=\d" with the next page's number and callback to parse
                 first_page = response.url.split("&")[0]
                 next_page = first_page + f"&page={page_num}"
                 if next_page is not None:
                     yield response.follow(next_page, callback=self.parse)
+            else:
+                break
 
     def save_estate(self, response):
         # construct json to save
@@ -57,5 +61,3 @@ class SrealitySpider(scrapy.Spider):
         doc_ref.set(data)
         # increment by one to stop after 2000 requests due to limit
         self.stopping_count += 1
-        self.cache.append(estate_id)
-        update_cache(self.cache)
