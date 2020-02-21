9a70c0a664e1f2ea0d5f3557e23720cb0bfe5275
Elisabeth Gabalová <elisabeth@Elisabeths-MacBook-Pro.local>
2020-02-21 21:09:03 +0100

    initial spider for saving all estates, still has not way of stopping the spider from crawling already crawled sites

--- sreality_scraping/sreality_scraping/spiders/sreality_spider.py
+++ sreality_scraping/sreality_scraping/spiders/sreality_spider.py
@@ -2,51 +2,53 @@ import scrapy
 import json
 import os
 from datetime import datetime
-from utils import load_cache, update_cache, connect_firestore, flatten_json
+from utils import connect_firestore, flatten_json
+import requests
+import logging
 
 
 class SrealitySpider(scrapy.Spider):
     name = "sreality"
-    per_page = 500
-    cache = load_cache()
-    db = connect_firestore("../notebooks/estates-b0e1b-f074b83c9111.json")
-    stopping_count = 0
+    _db = connect_firestore("../notebooks/estates-b0e1b-f074b83c9111.json")
+    _stopping_count = 0
 
     def start_requests(self):
-        url = (
-            f"https://www.sreality.cz/api/cs/v2/estates?per_page={self.per_page}&page=1"
-        )
-        yield scrapy.Request(url=url, callback=self.parse)
+        # catch sample response and extract result size to calculate how many pages we need to scrape to get all estates
+        # if there are 500 estates per page
+        r = requests.get("https://www.sreality.cz/api/cs/v2/estates?per_page=1&page=1")
+        results_count = r.json()["result_size"]
+        page_count = results_count // 500 + 1
+        urls = [
+            f"https://www.sreality.cz/api/cs/v2/estates?per_page=500&page={num}"
+            for num in range(page_count)
+        ]
+        for url in urls:
+            if self._stopping_count < 18293:
+                yield scrapy.Request(url=url, callback=self.parse)
+            else:
+                logging.info(f'Stopping count reached in start_requests. Stopping count: {self._stopping_count}.')
+                break
 
     def parse(self, response):
         r = json.loads(response.body)
-        # get number of pages as ... 'Number of total estates/number of estates per page'
-        page_count = r["result_size"] // self.per_page + 1
         # parsing links to estates in initial api response
         for estate in r["_embedded"]["estates"]:
             # url is provided as '/cs/v2/estates/1077595740'
             url_suffix = estate["_links"]["self"]["href"]
+            # check if ID in db.collection
             estate_id = url_suffix.split("/")[-1]
-            estate_exists = self.db.collection(u'sreality').document(estate_id).exists()
+            estate_exists = self._db.collection("sreality").document(estate_id).get().exists
             if not estate_exists:
-                # we combine it with the prefix from supplied url in self.start_requests
-                url_prefix = response.url.split("/cs")[0]
-                url_estate = url_prefix + url_suffix
-                if url_estate is not None:
-                    yield response.follow(url_estate, callback=self.save_estate)
-
-        # we need to generate new page after we scrape estates on the current one
-        # (2, page_count + 1) due to how range works and we already have the first page
-        for page_num in range(2, page_count):
-            # scrape only non-scraped estates
-            if self.stopping_count < 20000:
-                # split at "&page=\d" with the next page's number and callback to parse
-                first_page = response.url.split("&")[0]
-                next_page = first_page + f"&page={page_num}"
-                if next_page is not None:
-                    yield response.follow(next_page, callback=self.parse)
-            else:
-                break
+                if self._stopping_count < 18293:
+                    # we combine it with the prefix from supplied url in start_requests
+                    logging.info(f'Processing estate {estate_id}.')
+                    url_prefix = response.url.split("/cs")[0]
+                    url_estate = url_prefix + url_suffix
+                    if url_estate is not None:
+                        yield response.follow(url_estate, callback=self.save_estate)
+                else:
+                    logging.info(f'Stopping count reached in parse. Stopping count: {self._stopping_count}.')
+                    break
 
     def save_estate(self, response):
         # construct json to save
@@ -57,7 +59,8 @@ class SrealitySpider(scrapy.Spider):
         data = flatten_json(data)
         # create document reference within sreality collection in db
         estate_id = response.url.split("/")[-1]
-        doc_ref = self.db.collection("sreality").document(estate_id)
+        doc_ref = self._db.collection("sreality").document(estate_id)
         doc_ref.set(data)
-        # increment by one to stop after 2000 requests due to limit
-        self.stopping_count += 1
+        logging.info(f'Estate {estate_id} saved to collection.')
+        # increment by one to stop after 20000 requests due to limit
+        self._stopping_count += 1
